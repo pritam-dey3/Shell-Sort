8019cba6a8fd7f82ef6431b9c07f3065f05bf7d6
pritam3339 <pdeypritam@gmail.com>
2020-02-09 12:47:05 +0530

    improving sort plot

--- play plots.R
+++ play plots.R
@@ -19,9 +19,8 @@ avg <- function(funcs = list(), r1=20, r2=20, l = 100){
 random_sample = sample(1:10000)
 avg(c("SE86", "TO92", "CI01"))
 
-sort_plot <- function(n=4000, r1=20, r2=5, funcs = c("SE86", "TO92", "CI01")){
-  k = floor(log(n, 5)) + 1
-  itr <- sapply(3:k, function (x) 5^x)
+sort_plot <- function(k1=3, k2=5, r1=20, r2=5, funcs = c("SE86", "TO92", "CI01")){
+  itr <- sapply(k1:k2, function (x) 5^x)
   y <- numeric(length(funcs))
   names(y) <- funcs
   for (i in itr) {
@@ -38,7 +37,7 @@ sort_plot <- function(n=4000, r1=20, r2=5, funcs = c("SE86", "TO92", "CI01")){
       collapse="+"),
     "~itr")
   
-  write.csv(D, "data.csv")
+  write.csv(D, sprintf("data/data-%d-%d.csv", k1, k2))
   
   xyplot(as.formula(frml), data = D, 
          type=c('p', 'l'),
@@ -48,7 +47,7 @@ sort_plot <- function(n=4000, r1=20, r2=5, funcs = c("SE86", "TO92", "CI01")){
 }
 
 
-sort_plot(n = 2000, funcs = c("QuickSortH_WC", "RQsort"))
+sort_plot(k1=3, k2=5, funcs = c("QuickSortH_WC", "RQsort"))
 
 RQsort <- function(x) sort(x, method="quick")
 avg()
