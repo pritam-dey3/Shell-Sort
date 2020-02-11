ad79fd2943a2e6b00a325e50ae2b1219b41bec7d
pritam3339 <36327719+pritam3339@users.noreply.github.com>
2020-02-11 22:43:38 +0530

    comp plots delete old functions

--- SortPlot Comps.R
+++ SortPlot Comps.R
@@ -49,20 +49,3 @@ sort_plot1 <- function(seqn, r1=20, funcs = c("SE86", "TO92", "CI01")){
 seqn = seq(from=2, to=6, length=15)
 sort_plot1(seqn, funcs = c("CI01", "SE86", "TO92"))
 
-sort_plot1 <- function(n=4000, intv=50, r=100){
-  itr <- seq(10, n, intv)
-  y <- numeric(4)
-  for (i in itr) {
-    avg <- avg1(i)
-    log2nfact = sum(sapply(1:i, function(n) log2(n)))
-    y <- rbind(y, avg/(log2nfact))
-  }
-  D = as.data.frame(cbind(y[-1, ], itr))
-  xyplot(se86 + to92 + ci01 + qui ~ itr, data = D, 
-         type=c('p', 'l'),
-         auto.key = TRUE,
-         scales = list(x=list(log = 2))
-         )
-}
-
-sort_plot1()
