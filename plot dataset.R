db50eab74852f86436d8ba253bd0adfe7b8175ae
pritam3339 <36327719+pritam3339@users.noreply.github.com>
2020-02-11 23:45:32 +0530

    plotting from dataset

---
+++ plot dataset.R
@@ -0,0 +1,18 @@
+#plotting with dataset
+
+D1 <- read.csv("data/time -- QuickSortH_WC+QuickSortL_WC+CI01+SE86 -- 2.000000, 4.857143.csv", row.names=1)
+D2 <- read.csv("data/time -- QuickSortH_WC+QuickSortL_WC+CI01+SE86 -- 5.142857, 6.000000.csv", row.names=1)
+
+D <- rbind(D1, D2)
+
+frml = paste(
+  paste(
+    names(D)[-dim(D)[2]], 
+    collapse="+"),
+  "~itr")
+
+xyplot(as.formula(frml), data = D, 
+       type=c('p', 'l'),
+       auto.key = TRUE,
+       scales = list(x=list(log = 10))
+)
