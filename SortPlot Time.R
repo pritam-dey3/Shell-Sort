0b8c1dc4cfba4d193a9292e39ab12f98e1a38109
pritam3339 <36327719+pritam3339@users.noreply.github.com>
2020-02-11 21:33:33 +0530

    updating ploting function

--- SortPlot Time.R
+++ SortPlot Time.R
@@ -17,7 +17,7 @@ avg <- function(funcs = list(), r1=20, r2=20, l = 100){
 }
 
 sort_plot <- function(seqn, r1=20, r2=5, funcs = c("SE86", "TO92", "CI01")){
-  itr <- sapply(seqn, function (x) 5^x)
+  itr <- sapply(seqn, function (x) floor(10^x))
   y <- numeric(length(funcs))
   names(y) <- funcs
   for (i in itr) {
@@ -34,18 +34,18 @@ sort_plot <- function(seqn, r1=20, r2=5, funcs = c("SE86", "TO92", "CI01")){
       collapse="+"),
     "~itr")
   
-  write.csv(D, sprintf("data/time -- %s -- %s.csv", paste(funcs, collapse = "+"), paste(seqn, collapse = "-")))
+  write.csv(D, sprintf("data/time -- %s -- %f, %f.csv", paste(funcs, collapse = " + "), seqn[1], seqn[length(seqn)]))
   
   xyplot(as.formula(frml), data = D, 
          type=c('p', 'l'),
          auto.key = TRUE,
-         scales = list(x=list(log = 5))
+         scales = list(x=list(log = 10))
   )
 }
 
 
-sort_plot(3:6, funcs = c("QuickSortH_WC", "QuickSortL_WC", "CI01", "SE86"))
+sort_plot(seqn[1:11], funcs = c("QuickSortH_WC", "QuickSortL_WC", "CI01", "SE86"))
 
 RQsort <- function(x) sort(x, method="quick")
 
-                seqn <- c(2, 2.28571428571429, 2.57142857142857, 2.85714285714286, 3.14285714285714, 3.42857142857143, 3.71428571428571, 4, 4.28571428571429, 4.57142857142857, 4.85714285714286, 5.14285714285714, 5.42857142857143, 5.71428571428571, 6)
+seqn <- c(2, 2.28571428571429, 2.57142857142857, 2.85714285714286, 3.14285714285714, 3.42857142857143, 3.71428571428571, 4, 4.28571428571429, 4.57142857142857, 4.85714285714286, 5.14285714285714, 5.42857142857143, 5.71428571428571, 6)
